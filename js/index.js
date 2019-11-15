71c31174adea105657c8117fdfe7c695021e4091
TanJiChang <1771357408@qq.com>
2019-11-15 08:48:31 +0800

    v01.01

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//配置
+// require.config({
+//     paths:{
+//         jquery:"../node_modeules/jquery/dist/jquery.min",
+//         app:"./app"
+//     }
+// });
+// require(['app'],function(app){
+    
+// })

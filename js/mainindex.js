d778ad550c5ab33ea326d3dd42d2e36b7d6759f4
TanJiChang <1771357408@qq.com>
2019-11-16 08:42:34 +0800

    v01.02

---
+++ js/mainindex.js
@@ -0,0 +1,11 @@
+//配置
+require.config({
+    paths:{
+        jquery:"../node_modules/jquery/dist/jquery.min",
+        bootrstrap:"../node_modules/bootstrap/dist/js/bootstrap.min",
+        //index:"./lib/index",
+    },
+    shim:{}
+});
+
+
